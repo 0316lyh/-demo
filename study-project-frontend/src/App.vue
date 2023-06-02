7e013258e322ee8e00b418ce3b02d28c10d3ccc9
0316lyh <2042971411@qq.com>
2023-06-02 22:02:20 +0800

    test

--- study-project-frontend/src/App.vue
+++ study-project-frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'1
+import HelloWorld from './components/HelloWorld.vue'
 // test
 </script>
 
